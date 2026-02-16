534d076d8c2a46b253d91af514ae11657fb2cddc
Shubham Kumar <142138865+singhshubham23@users.noreply.github.com>
2026-02-16 20:03:11 +0530

    Remove unnecessary blank lines in hotspotRoutes.js

--- routes/hotspotRoutes.js
+++ routes/hotspotRoutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/hotspotController");
-
 router.get("/", controller.getAccidentHotspots);
-
 module.exports = router;
